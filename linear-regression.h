0769dc7f66116ada0c2d6a53778263944c98f34e
Piotr Kozubek <piotrkozubek@gmail.com>
2018-10-18 15:02:32 +0200

    Moved linear regression to header file

--- linear-regression.h
+++ linear-regression.h
@@ -25,4 +25,82 @@ std::vector<double> gradientDescentIterativeVersion(const featuresSet &X, const 
   return t;
 }
 
+void linearRegressionCodeIterative()
+{
+  std::vector<double> y;
+  std::string octaveHeader = "# Created by Octave 3.8.2, Fri Oct 12 16:32:14 2018 CEST <piotrkozubek@gmail.com>";
+  featuresSet x;
+  std::cout << "Iterative version of gradient descent" << std::endl;
+  std::vector<double> thetas = {0, 0, 0, 0, 0};
+  int m = 5; // training sets
+  int n = 5; // features size
+  double alpha = 0.0005;
+  int featuresSize = thetas.size() - 1;
+  std::vector<double> HVector;
+
+  featuresRow row;
+
+  for (int k = 0; k < m; ++k)
+  {
+    row.clear();
+    row.push_back(1);
+    for (int f = 0; f < featuresSize; ++f)
+    {
+      row.push_back(k + 1);
+    }
+    x.push_back(row);
+  }
+
+  // double min = 1;
+  // double max = 10;
+  // scaleFeatures(x, min, max);
+
+  for (int i = 0; i < m; ++i)
+  {
+    y.push_back(i + 2);
+    print(x[i]);
+  }
+  std::cout << std::endl;
+  print(y);
+
+  std::vector<double> calculatedThetas;
+  calculatedThetas = gradientDescentIterativeVersion(x, y, thetas, alpha);
+  std::cout << "Calculated thetas" << std::endl;
+  print(calculatedThetas);
+  std::cout << std::endl
+            << std::endl;
+}
+
+void linearRegressionCodeVectorized()
+{
+  std::cout << "Vectorized Gradient descent" << std::endl;
+  int m = 5; // training sets
+  int n = 5; // features size
+
+  Eigen::MatrixXd theta(m, 1);
+  Eigen::MatrixXd X(m, n);
+  Eigen::MatrixXd Y(m, 1);
+
+  for (int i = 0; i < m; ++i)
+  {
+    Y(i, 0) = i + 2;
+    for (int j = 0; j < n; ++j)
+    {
+      X(i, j) = i + 1;
+    }
+    X(i, 0) = 1;
+  }
+
+  std::cout << std::endl;
+  std::cout << Y << std::endl;
+  std::cout << std::endl;
+  std::cout << X << std::endl;
+  std::cout << std::endl;
+  theta = normalEquation(X, Y);
+  std::cout << "Calculated thetas" << std::endl;
+  std::cout << theta << std::endl;
+  std::cout << std::endl
+            << std::endl;
+}
+
 #endif
--- main.cpp
+++ main.cpp
@@ -14,83 +14,13 @@
 #include "utils.h"
 #include "linear-regression.h"
 
-std::vector<double> y;
-std::string octaveHeader = "# Created by Octave 3.8.2, Fri Oct 12 16:32:14 2018 CEST <piotrkozubek@gmail.com>";
-
-featuresSet x;
-
 int main()
 {
   try
   {
     std::cout << "Squared cost function v.0.0.1" << std::endl;
-    std::cout << "Vectorized Gradient descent" << std::endl;
-    int m = 5; // training sets
-    int n = 5; // features size
-
-    Eigen::MatrixXd theta(m, 1);
-    Eigen::MatrixXd X(m, n);
-    Eigen::MatrixXd Y(m, 1);
-
-    for (int i = 0; i < m; ++i)
-    {
-      Y(i, 0) = i + 2;
-      for (int j = 0; j < n; ++j)
-      {
-        X(i, j) = i + 1;
-      }
-      X(i, 0) = 1;
-    }
-
-    std::cout << std::endl;
-    std::cout << Y << std::endl;
-    std::cout << std::endl;
-    std::cout << X << std::endl;
-    std::cout << std::endl;
-    theta = normalEquation(X, Y);
-    std::cout << "Calculated thetas" << std::endl;
-    std::cout << theta << std::endl;
-    std::cout << std::endl
-              << std::endl;
-
-    std::cout << "Iterative version of gradient descent" << std::endl;
-    std::srand(time(nullptr));
-    std::vector<double> thetas = {0, 0, 0, 0, 0};
-    double alpha = 0.0005;
-    int featuresSize = thetas.size() - 1;
-    std::vector<double> HVector;
-
-    featuresRow row;
-
-    for (int k = 0; k < m; ++k)
-    {
-      row.clear();
-      row.push_back(1);
-      for (int f = 0; f < featuresSize; ++f)
-      {
-        row.push_back(k + 1);
-      }
-      x.push_back(row);
-    }
-
-    // double min = 1;
-    // double max = 10;
-    // scaleFeatures(x, min, max);
-
-    for (int i = 0; i < m; ++i)
-    {
-      y.push_back(i + 2);
-      print(x[i]);
-    }
-    std::cout << std::endl;
-    print(y);
-
-    std::vector<double> calculatedThetas;
-    calculatedThetas = gradientDescentIterativeVersion(x, y, thetas, alpha);
-    std::cout << "Calculated thetas" << std::endl;
-    print(calculatedThetas);
-    std::cout << std::endl
-              << std::endl;
+    linearRegressionCodeIterative();
+    linearRegressionCodeVectorized();
 
     // SAVING FILE FOR OCTAVE
 
